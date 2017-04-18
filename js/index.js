248092df93af1bf089168b4f6c4bbef91d709dcb
Ivan <pysoccerp@hotmail.com>
2017-04-18 01:16:14 -0400

    Trying the jQuery when method for sorting

--- js/index.js
+++ js/index.js
@@ -1,17 +1,23 @@
 
-var gamertags = ['JohnLocke15', 'Yemen3', 'StizNewton', 'TheDailyDriver', 'Monohloco', 'iCellyHard17'];
-    row = '';
-
-$.each(gamertags, function(index, gamertag) {
-$.getJSON('https://api.r6stats.com/api/v1/players/' + gamertag + '?platform=xone', function(data) {
-    row += '<tr><td>' + data.player.username + '</td><td>' + data.player.stats.ranked.kd + '</td><td>' + data.player.stats.ranked.wlr + '</td><td>' + data.player.stats.progression.level + '</td><td>' + data.player.stats.ranked.kills + '</td><td>' + data.player.stats.ranked.deaths + '</td><td>' + data.player.stats.ranked.wins + '</td><td>' + data.player.stats.ranked.losses + '</td></tr>';
-    $('.stats').html(row);
-    });
-    $("#myTable").tablesorter();
-});
 
 $('document').ready(function() {
-    $("#myTable").trigger("update");
-    var sorting = [[0,0]];
-    $("#myTable").trigger("sorton",[sorting]);
+    var gamertags = ['JohnLocke15', 'Yemen3', 'StizNewton', 'TheDailyDriver', 'Monohloco', 'iCellyHard17'];
+        row = '';
+
+    $.when(makeTables()).done(function(doIt) {
+        $("#myTable").tablesorter();
+    });
+
+    function makeTables() {
+        return $.each(gamertags, function(index, gamertag) {
+        $.getJSON('https://api.r6stats.com/api/v1/players/' + gamertag + '?platform=xone', function(data) {
+            row += '<tr><td>' + data.player.username + '</td><td>' + data.player.stats.ranked.kd + '</td><td>' + data.player.stats.ranked.wlr + '</td><td>' + data.player.stats.progression.level + '</td><td>' + data.player.stats.ranked.kills + '</td><td>' + data.player.stats.ranked.deaths + '</td><td>' + data.player.stats.ranked.wins + '</td><td>' + data.player.stats.ranked.losses + '</td></tr>';
+            $('.stats').html(row);
+            });
+        });
+    }
 });
+
+    // $("#myTable").trigger("update");
+    // var sorting = [[0,0]];
+    // $("#myTable").trigger("sorton",[sorting]);
