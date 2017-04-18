87b50601f66e6c96d24f0fec22b12d9a1e1fdc09
Ivan <pysoccerp@hotmail.com>
2017-04-18 01:04:00 -0400

    Added update trigger for sorting

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
-$('document').ready(function() {
-  var gamertags = ['JohnLocke15', 'Yemen3', 'StizNewton', 'TheDailyDriver', 'Monohloco', 'iCellyHard17'];
-      row = '';
 
-  $.each(gamertags, function(index, gamertag) {
-    $.getJSON('https://api.r6stats.com/api/v1/players/' + gamertag + '?platform=xone', function(data) {
-        row += '<tr><td>' + data.player.username + '</td><td>' + data.player.stats.ranked.kd + '</td><td>' + data.player.stats.ranked.wlr + '</td><td>' + data.player.stats.progression.level + '</td><td>' + data.player.stats.ranked.kills + '</td><td>' + data.player.stats.ranked.deaths + '</td><td>' + data.player.stats.ranked.wins + '</td><td>' + data.player.stats.ranked.losses + '</td></tr>';
-        $('.stats').html(row);
-      });
-    console.log($("#myTable tbody tr").length);
-  });
+var gamertags = ['JohnLocke15', 'Yemen3', 'StizNewton', 'TheDailyDriver', 'Monohloco', 'iCellyHard17'];
+    row = '';
 
+$.each(gamertags, function(index, gamertag) {
+$.getJSON('https://api.r6stats.com/api/v1/players/' + gamertag + '?platform=xone', function(data) {
+    row += '<tr><td>' + data.player.username + '</td><td>' + data.player.stats.ranked.kd + '</td><td>' + data.player.stats.ranked.wlr + '</td><td>' + data.player.stats.progression.level + '</td><td>' + data.player.stats.ranked.kills + '</td><td>' + data.player.stats.ranked.deaths + '</td><td>' + data.player.stats.ranked.wins + '</td><td>' + data.player.stats.ranked.losses + '</td></tr>';
+    $('.stats').html(row);
+    });
+    $("#myTable").tablesorter();
 });
-console.log($("#myTable tbody tr").length);
-if ($("#myTable tbody tr").length > 0)
-        $("#myTable").tablesorter({ sortList: [[0,0]]});
-//   $("#myTable").tablesorter();
+
+$('document').ready(function() {
+    $("#myTable").trigger("update");
+    var sorting = [[0,0]];
+    $("#myTable").trigger("sorton",[sorting]);
+});
